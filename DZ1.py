56d8f570102ddc5120a0055d0b77d59e29758dfb
JuliyaCiktor <julia_uk_oleksandrivna@ukr.net>
2024-08-17 17:06:54 +0300

    Added example

--- DZ1.py
+++ DZ1.py
@@ -1,4 +1,3 @@
-#Первый вариант
 number = int (input("Please enter a four digit number: ")) #1234
 intermediate_number=number//10 #123
 # print(intermediate_number)
@@ -10,7 +9,7 @@ number_three = intermediate_number%10 #3
 print(number_three)
 number_four = number%10 #4
 print(number_four)
-#Второй вариант
+
 # first_number=number%10
 # print(first_number)
 # number_two=intermediate_number%10
--- DZ2(2_2).py
+++ DZ2(2_2).py
@@ -11,6 +11,9 @@ number4 = n3//10%10
 print(number4)
 number5 = number%10
 print(number5)
+#1
 print(f"{number5}{number4}{number3}{number2}{numder1}")
+#2
 print(number5,number4,number3,number2,numder1)
+#3
 print(number5*10000+number4*1000+number3*100+number2*10+numder1)
